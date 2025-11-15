2bb451f11c1ca5516961aebd8a4de4733916fe15
IAmJustAmateur <ig.hizhnyak@gmail.com>
2025-11-15 13:18:28 +0300

    some refactoring

--- agent_api/urls.py
+++ agent_api/urls.py
@@ -25,4 +25,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("demo/", include("demo_interface.urls")),
-] + debug_toolbar_urls()
+]
+
+from debug_toolbar.toolbar import debug_toolbar_urls
+
+urlpatterns += debug_toolbar_urls()
